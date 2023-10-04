a1256e0b4af8210ddfdbee9189e0fe6a24129da1
gadkins <grayson.adkins@uffizzi.com>
2023-10-04 10:01:56 -0500

    configuration update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'));
-app.get('/hello', (req, res) => res.send('Hello World'))
+app.get('/hello', (req, res) => res.send('Hello Uffizzi'))
 
 const port = 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
