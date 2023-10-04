c06b11d6f138fb9f8485f85a3d4e5c436a467681
Zipofar <zipofar@gmail.com>
2023-10-04 19:25:39 +0300

    Update skaffold

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'));
+app.get('/', (req, res) => res.send('Home Page'))
 app.get('/hello', (req, res) => res.send('Hello Uffizzi'))
 
-const port = 3000
+const port = 8080
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
